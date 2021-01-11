a4fe95013471edf49c3e8a7f476006662239a8ac
Edward Alexis <edalvb@gmail.com>
2021-01-11 10:26:23 -0500

    configuración inicial

---
+++ jerstrapi/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'a885ff9f0ec04113bfe421d718055ce3'),
+    },
+  },
+});
